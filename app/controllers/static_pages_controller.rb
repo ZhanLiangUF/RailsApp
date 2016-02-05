253c08bf8371d8e9e6b6d5da72ff64773a4c77f7
zhanlianguf <zliang@ufl.edu>
2016-02-05 06:54:22 +0000

    add microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,19 +1,11 @@
 class StaticPagesController < ApplicationController
 
-<<<<<<< HEAD
-    def home
-        @micropost = current_user.microposts.build if logged_in?
-        @feed_items = current_user.feed.paginate(page: params[:page])
-    end 
-    
-=======
    def home
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
->>>>>>> user-microposts
     def help
     end
     def about
